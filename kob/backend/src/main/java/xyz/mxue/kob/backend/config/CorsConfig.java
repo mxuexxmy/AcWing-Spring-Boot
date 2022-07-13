d368cd741eb7fb74853a287a019b1a32b517dd6c
mxuexxmy <f9ab5014>
2022-07-13 22:26:12 +0800

    item initial

---
+++ kob/backend/src/main/java/xyz/mxue/kob/backend/config/CorsConfig.java
@@ -0,0 +1,51 @@
+package xyz.mxue.kob.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * <h2> CorsConfig 跨域配置 </h2>
+ *
+ * @author mxuexxmy
+ * @date 2022/7/12 19:16
+ */
+@Configuration
+public class CorsConfig implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        String origin = request.getHeader("Origin");
+        if(origin!=null) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
+        }
+
+        String headers = request.getHeader("Access-Control-Request-Headers");
+        if(headers!=null) {
+            response.setHeader("Access-Control-Allow-Headers", headers);
+            response.setHeader("Access-Control-Expose-Headers", headers);
+        }
+
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        filterChain.doFilter(request, response);
+
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
